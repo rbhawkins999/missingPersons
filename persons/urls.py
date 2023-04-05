dabbdcb59163ee554b1b642dd68e3e1ca8eecbcb
rbhawkins999 <rbhawkins999@gmail.com>
2023-04-04 18:49:17 -0600

    added into the urls in the application

--- persons/urls.py
+++ persons/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView tablePageView aboutPageView
 
 urlpatterns = [
-    path("", indexPageView, name = "index")
+    path("", indexPageView, name = "index"),
+    path("table/", tablePageView, name = "table"),
+    path("about/", aboutPageView, name = "about")
 ]
