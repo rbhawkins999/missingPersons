f838ccd73037af1c92d6ad07393824d4162afc75
rbhawkins999 <rbhawkins999@gmail.com>
2023-04-04 18:56:01 -0600

    ryan commit

--- persons/urls.py
+++ persons/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexPageView tablePageView aboutPageView
+from .views import indexPageView, tablePageView, aboutPageView
 
 urlpatterns = [
     path("", indexPageView, name = "index"),
